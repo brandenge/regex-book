084312b74b9a8bf558cb8cb9567b936fc9ac0c31
Branden Ge <brandenge@protonmail.com>
2021-11-23 10:36:47 -0600

    Exercises completed for Chapter 1: Basic Matching.

---
+++ 1-basic-matching/2.rb
@@ -0,0 +1,9 @@
+=begin
+/h/i
+
+or
+
+/(h|H)/
+
+If you don't want the entire regex to be case sensitive, then you would want to use alternation for just the "h", instead of the i option/modifier which would affect the entire regex.
+=end
---
+++ 1-basic-matching/6.rb
@@ -0,0 +1,5 @@
+=begin
+/(blue|black)berry/
+
+The regex doesn't match "black berry" because there is no space character included in the regex. The space character counts as a character just like any other letter, number, or symbol character.
+=end
